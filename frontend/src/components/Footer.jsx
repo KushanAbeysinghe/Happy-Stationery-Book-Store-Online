99ebbf1873bda04076ac11d5a5adcd6d9ed402af
Kushan Abeysinghe <kushjayamal@gmail.com>
2024-07-22 21:18:50 +0530

    Footer details update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,19 +9,19 @@ const Footer = () => {
         <div className="row">
           <div className="col-md-3 logo">
             <img src={logo} alt="Happy Store" />
-            <p>TicketsMinistry is one of the fast-growing ticket marketplaces that includes events of music, sport, art, theatre, and more. It is capable to accommodate events of all types, sizes, and complexities with state-of-the-art technology.</p>
+            <p>Welcome to Happy Store, your one-stop shop for all your book and stationery needs. From the latest bestsellers to essential office supplies, we have everything to keep you inspired and organized. Explore our wide range of products and discover the joy of shopping with Happy Store.</p>
             <div className="social-icons">
-              <a href="#"><FaFacebookF /></a>
-              <a href="#"><FaInstagram /></a>
-              <a href="#"><FaWhatsapp /></a>
-              <a href="#"><FaLinkedinIn /></a>
-              <a href="#"><FaEnvelope /></a>
+              <a href="https://www.facebook.com/profile.php?id=61558947222960&mibextid=LQQJ4d"><FaFacebookF /></a>
+              <a href="https://www.instagram.com/happy_stationery01?igsh=MW8xcW5nY2NhenhiYg=="><FaInstagram /></a>
+              <a href="https://wa.me/message/G5LOKX4KZ5QXC1?src=qr"><FaWhatsapp /></a>
+              {/* <a href="#"><FaLinkedinIn /></a>
+              <a href="#"><FaEnvelope /></a> */}
             </div>
           </div>
           <div className="col-md-3">
             <h5>Site Map</h5>
             <ul className="list-unstyled">
-              <li><a href="/bookstore">Book Store</a></li>
+              <li><a href="/">Book Store</a></li>
               <li><a href="/stationery">Stationery Store</a></li>
             </ul>
           </div>
@@ -35,10 +35,10 @@ const Footer = () => {
           <div className="col-md-3">
             <h5>Contact</h5>
             <ul className="list-unstyled">
-              <li><a href="tel:+94777389172"><FaPhone /> +94777389172</a></li>
-              <li><a href="tel:+94777371672"><FaPhone /> +94777371672</a></li>
-              <li><a href="https://maps.google.com?q=No.192, Maladola Wattha, Kadawatha."><FaMapMarkerAlt /> No.192, Maladola Wattha, Kadawatha.</a></li>
-              <li><a href="mailto:hello@ticketsministry.com"><FaEnvelope /> hello@ticketsministry.com</a></li>
+              <li><a href="tel:+94743632710"><FaPhone />+94743632710</a></li>
+              {/* <li><a href="tel:+94777371672"><FaPhone /> +94777371672</a></li> */}
+              <li><a href=""><FaMapMarkerAlt /> No. 09, Lilian Avenue, Mount Lavinia</a></li>
+              <li><a href=" contact@happystationerynbooks.lk"><FaEnvelope /> contact@happystationerynbooks.lk</a></li>
             </ul>
           </div>
         </div>
